f0a68065ab23bfb84c587b1eb801b340038718bf
aquiladive <102308614+aquiladive@users.noreply.github.com>
2022-11-13 13:11:31 +0530

    three basic programs
    hello world, arithmetic operations and swapping variable values

---
+++ Basics/Swap.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter two numbers.\n");
+    scanf("%d %d",&a,&b);
+    printf("Before swapping, a = %d, b = %d\n",a,b);
+    //with temp variable
+    c=a;
+    a=b;
+    b=c;
+    printf("After first swap, a = %d, b = %d\n",a,b);
+    //without temp variable
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("After second swap, a = %d, b = %d\n",a,b);
+}
